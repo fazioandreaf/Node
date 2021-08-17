cd3ed557f707f6f40d77c56445ac4933ea9fe9de
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-08-17 16:51:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// va a richeider il modulo che permette di usare funzioni che verificano gli invcarianti
+const assert = require('assert').strict;
+// vado a richiedere questo modulo
+const http = require('http');
+
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
